a32aff1441f326c2e9842211eba5d161d4ebeec5
Psytor <psytor@frmdev.lan>
2025-06-03 15:04:48 +0000

    Dev Configuration 	modified:   vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/mod-evaluator/',
+  base: '/mod-eval-dev/',
   server: {
     host: '0.0.0.0', // Listen on all network interfaces
     port: 5173,      // You can change this if needed
